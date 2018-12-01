982f8c5b3187eb202120424ce6e13d685ce3ecd6
shamikulamin <shamikulamin@gmail.com>
2018-11-30 21:17:25 -0600

    added save review endpoint

--- buyrgames/src/main/java/com/revature/buyrgames/controllers/ReviewController.java
+++ buyrgames/src/main/java/com/revature/buyrgames/controllers/ReviewController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +33,10 @@ public class ReviewController {
 	@GetMapping("{id}")
 	public List<Review> findByProductId(@PathVariable int id){
 		return rs.findByProductId(id);
+	}
+	
+	@PostMapping
+	public int save(@RequestBody Review r) {
+		return rs.save(r);
 	}
 }
